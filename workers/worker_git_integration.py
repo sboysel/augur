65c67de7be1288e884f784f60aa1fcd55d843fcb
Sean P. Goggins <s@goggins.com>
2022-06-11 16:31:53 -0500

    error logging not behaving how I would expect.

--- workers/worker_git_integration.py
+++ workers/worker_git_integration.py
@@ -216,18 +216,16 @@ class WorkerGitInterfaceable(Worker):
 
         # 1 second of sleep just to dial back how hard we are hitting the API a little bit. 
         # In practice we are hammering hard. Subseconds of pause. A second may help a lot.
-        try: 
-            self.logger.debug('got to 1 second sleep.\n')
-            sleep(1)
-            '''
-            For every token returned by the query we are trying to process and understand which token is the most "fresh" and available for the worke we are about to do. 
+        self.logger.debug('got to 1 second sleep.\n')
+        sleep(1)
+        '''
+        For every token returned by the query we are trying to process and understand which token is the most "fresh" and available for the worke we are about to do. 
+        self.logger.debug(f"Exception: {e}.")
+        stacker = traceback.format_exc()
+        self.logger.debug(f"{stacker}")
+        continue       
 
-            '''
-        except Exception as e: 
-            self.logger.debug(f"Exception: {e}.")
-            stacker = traceback.format_exc()
-            self.logger.debug(f"{stacker}")
-            continue
+        '''
 
         self.logger.debug(f'Printing the oauth for loop values. Seems to get stuck here. {self.config[key_name]}\n ')
         for oauth in [{'oauth_id': 0, 'access_token': self.config[key_name]}] + json.loads(
